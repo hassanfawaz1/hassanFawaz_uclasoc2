8a117491e79d9d78e354a5365adca20d32eb2805
hassan fawaz <hassanfawaz2002@gmail.com>
2018-07-24 10:58:11 -0700

    Update navbar

---
+++ java
@@ -0,0 +1,8 @@
+var animal = ['zebra', 'line', 'tiger', 'elephant', 'monkey'];
+var largestAnimalName = animals.filter (animal => animal.length > 4
+
+var professionals = ['doctor', 'lawyer', 'programmer', 'ballerina'];
+var capitalize=professions.map( profession.toUpperCase());
+
+var professions = ['doctor', 'lawyer', 'programmer', 'ballerina'];
+var capitalize =professions.map ((profession => profession.toUpperCase());;
---
+++ loops.js
@@ -0,0 +1,15 @@
+var animal = ['zebra', 'line', 'tiger', 'elephant', 'monkey'];
+var largestAnimalName = animals.filter (animal => animal.length > 4
+
+var professionals = ['doctor', 'lawyer', 'programmer', 'ballerina'];
+var capitalize=professions.map( profession.toUpperCase());
+
+var professions = ['doctor', 'lawyer', 'programmer', 'ballerina'];
+var capitalize =professions.map (profession => profession.toUpperCase());
+
+var students = ['Emma', 'Yang', 'PepeJ', 'PepeV', 'Ethan', 'Alison', 'Nico', 'Brian', 'Uli', 'Caleb', 'Rawad', 'Hassan', 'Yueyang', 'Lyla']
+var awesome =students.map(student => student + " Rawad is awesome");
+
+var numbers = [12, 457, 52, 16];
+var total = numbers.reduce((memo,CurrentValue)=> memo + CurrentValue, 0);
+
